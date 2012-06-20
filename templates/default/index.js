6a73656fe11fd00f4a8a04358a312f74351f70b0
zeekay <zeekayy@gmail.com>
2012-06-19 20:14:39 -0500

    Update default template.

--- templates/default/index.js
+++ templates/default/index.js
@@ -1,9 +1,11 @@
-var app = require('die')({
+var die = require('die')({
   basePath: __dirname
 });
+
+var app = new die.createServer();
 
 if (!module.parent) {
   app.run();
 } else {
-  module.exports = app.createServer();
+  module.exports = app;
 }
